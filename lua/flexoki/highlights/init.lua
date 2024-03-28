f6376c19d504e9705bb7a0d890b7b1d65a807b57
Towry <towry@users.noreply.github.com>
2024-03-28 22:15:51 +0800

    fix: treesitter highlights

--- lua/flexoki/highlights/init.lua
+++ lua/flexoki/highlights/init.lua
@@ -2,19 +2,17 @@ local M = {}
 
 ---@param opts FlexokiOptions
 M.groups = function(opts)
-	-- This could be done dynamically by looking for all files, but this approach is fine and is safer
 	local modules = {
 		require("flexoki.highlights.base").groups(opts),
 		require("flexoki.highlights.mini-nvim").groups(),
 		require("flexoki.highlights.flash-nvim").groups(),
 		require("flexoki.highlights.neotree").groups(),
-		-- require('flexoki.highlights.buffer').groups(),
 		-- require('flexoki.highlights.cmp').groups(),
 		require("flexoki.highlights.git").groups(),
 		require("flexoki.highlights.lsp").groups(opts),
 		require("flexoki.highlights.markdown").groups(),
 		require("flexoki.highlights.telescope").groups(),
-		-- require("flexoki.highlights.treesitter").groups(),
+		require("flexoki.highlights.treesitter").groups(),
 		require("flexoki.highlights.coc").groups(),
 	}
 
